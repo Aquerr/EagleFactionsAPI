bb2334ca3d9fc857dacfbea1d18ec12551be0d81
Aquerr <nerdianin@gmail.com>
2020-06-04 10:15:54 +0200

    Change documentation for isCreateByItems method in FactionCreateEvent

--- src/main/java/io/github/aquerr/eaglefactions/api/events/FactionCreateEvent.java
+++ src/main/java/io/github/aquerr/eaglefactions/api/events/FactionCreateEvent.java
@@ -1,5 +1,6 @@
 package io.github.aquerr.eaglefactions.api.events;
 
+import io.github.aquerr.eaglefactions.api.config.FactionsConfig;
 import io.github.aquerr.eaglefactions.api.entities.Faction;
 import org.spongepowered.api.entity.living.player.Player;
 
@@ -9,9 +10,11 @@ import org.spongepowered.api.entity.living.player.Player;
 public interface FactionCreateEvent extends FactionEvent
 {
     /**
-     * <strong>Currently unimplemented</strong>
+     * Checks if the given faction has been created by using items.
      *
-     * @return <tt>true</tt> if faction is being created by items or <tt>false</tt> if it is not
+     * Checking {@link FactionsConfig#getFactionCreationByItems()} is equivalent to using this method.
+     *
+     * @return <tt>true</tt> if faction is being created by items or <tt>false</tt> if it is not.
      */
     boolean isCreatedByItems();
 }
