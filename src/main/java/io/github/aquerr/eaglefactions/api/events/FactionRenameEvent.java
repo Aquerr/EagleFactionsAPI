93aee97fa0f575f2e35d5be11339e47fd75f4885
Aquerr <nerdianin@gmail.com>
2020-08-16 20:22:09 +0200

    Fix FactionRenameEvent

--- src/main/java/io/github/aquerr/eaglefactions/api/events/FactionRenameEvent.java
+++ src/main/java/io/github/aquerr/eaglefactions/api/events/FactionRenameEvent.java
@@ -1,2 +1,6 @@
-package io.github.aquerr.eaglefactions.api.events;public interface FactionRenameEvent {
+package io.github.aquerr.eaglefactions.api.events;
+
+public interface FactionRenameEvent extends FactionEvent
+{
+    String getNewFactionName();
 }
